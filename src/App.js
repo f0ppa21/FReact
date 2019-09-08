0dc91a8d57a1e3990ab5718e9e657954d54750ea
Jonas Forsberg <jonas.forsberg@gmail.com>
2019-09-08 21:54:49 +0200

    Sid 58

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walker',
+    num_comments: 39,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clarks',
+    num_comments: 2,
+    points: 58,
+    objectID: 1
+  },
+];
+
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
   render() {
-    const list = [
-      {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-      },
-      {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clarks',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-      },
-    ];
+    const { searchTerm, list } = this.state;
 
     return (
       <div className="App">
-        {list.map(item =>
+        <form>
+          <input 
+            type="text" 
+            onChange={this.onSearchChange}
+            value={searchTerm}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -32,6 +64,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>

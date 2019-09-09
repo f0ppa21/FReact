8ca56168301b707cc9edb332ae09ed5d32a3fc24
Jonas Forsberg <jonas.forsberg@gmail.com>
2019-09-09 15:29:57 +0200

    Bla

--- src/App.js
+++ src/App.js
@@ -50,8 +50,8 @@ class App extends Component {
     return (
       <div className="App">
         <form>
-          <input 
-            type="text" 
+          <input
+            type="text"
             onChange={this.onSearchChange}
             value={searchTerm}
           />
@@ -67,6 +67,51 @@ class App extends Component {
             <span>
               <button
                 onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss

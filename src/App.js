d21799f44bea990a0d460ed277a4e2ef49ff59a2
Jonas Forsberg <jonas.forsberg@gmail.com>
2019-09-13 16:55:14 +0200

    Client search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walker',
-    num_comments: 39,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clarks',
-    num_comments: 2,
-    points: 58,
-    objectID: 1
-  },
-];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -28,15 +14,18 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
 
     return (
       <div className="page">
@@ -48,11 +37,13 @@ class App extends Component {
             Search
         </Search>
         </div>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        {result &&
+           <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />          
+        }
       </div>
     );
   }
@@ -60,8 +51,24 @@ class App extends Component {
   onSearchChange = (event) => this.setState({ searchTerm: event.target.value });
 
   onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: { ...this.state.result, hits: updatedHits }
+    });
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 }
 
@@ -81,7 +88,7 @@ const Table = ({ list, pattern, onDismiss }) => {
   const midColumn = { width: '30%' };
   const smallColumn = { width: '10%' };
 
-  return (
+  return (    
     <div className="table">
       {list.filter(isSearched(pattern)).map(item =>
         <div key={item.objectID} className="table-row">
